6d6c15cf9775508acbf25b16b6ef3c3341bc01de
angelOlvera277 <angel.olivera@physmodo.com>
2024-12-09 19:59:11 -0600

    Initial Commit

---
+++ raven/ravenApp.swift
@@ -0,0 +1,32 @@
+//
+//  ravenApp.swift
+//  raven
+//
+//  Created by Angel Olvera on 09/12/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ravenApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ravenTests/ravenTests.swift
@@ -0,0 +1,16 @@
+//
+//  ravenTests.swift
+//  ravenTests
+//
+//  Created by Angel Olvera on 09/12/24.
+//
+
+import Testing
+
+struct ravenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
